836ef7e25c0ef55553a01b942ad315cf8754e37b
xue99999 <1090374167@qq.com>
2018-05-30 00:06:30 +0800

    change file name

--- app.py
+++ app.py
@@ -2,22 +2,50 @@
 
 import os
 import json
+from datetime import datetime
 from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['TEMPLATES_NOT_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/shiyanlou'
+
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, title, content, category, created_time=None):
+        self.title = title
+        self.content = content
+        self.category = category
+
+        if created_time is None:
+            created_time = datetime.utcnow()
+        self.created_time = created_time
+            
+
+    def __repr__(self):
+        return '<File %r>' % self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 base = '/home/shiyanlou/files'
 
-def _get_list():
-    json_files = []
-    for x in os.listdir(base):
-        name, type = x.split('.')
-        if type == 'json':
-            json_files.append(os.path.join(base, x))
-        else:
-            print('file is not json type')
-    return json_files
 
 @app.errorhandler(404)
 def not_found(error):
@@ -25,28 +53,30 @@ def not_found(error):
 
 @app.route('/')
 def index():
+    file = File.query.all()
     
-    json_files = _get_list()
-    if len(json_files) <= 0:
-        abort(404)
-    text_list = []
-    for file in json_files:
-        with open(file) as f:
-            dct = json.loads(f.read())
-            text_list.append(dct) 
+    return render_template('index.html', names=file)
 
-    return render_template('index.html', names=text_list)
-
-@app.route('/files/<filename>')
-def file(filename):
-    filename = filename + '.json'
-    file = os.path.join(base, filename)
-
-    if os.path.exists(file):
-        with open(file) as f:
-            dct = json.loads(f.read())
+@app.route('/files/<file_id>')
+def file(file_id):
+    file = File.query.filter_by(id=file_id).first()
+    if file: 
+        article = file
     else:
         abort(404)
 
-    return render_template('file.html', article=dct)
+    return render_template('file.html', article=article)
 
+'''
+create table and data
+db.create_all()
+java = Category('Java')
+python = Category('Python')
+file1 = File('Hello Java', 'File Content - Java is cool!', java)
+file2 = File('Hello Python', 'File Content - Python is cool!', python)
+db.session.add(java)
+db.session.add(python)
+db.session.add(file1)
+db.session.add(file2)
+db.session.commit()
+'''
