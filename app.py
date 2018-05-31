6f538dcb838bae0fd8da7b9250868199025279ed
xue99999 <1090374167@qq.com>
2018-05-31 23:41:20 +0800

    change app and template

--- app.py
+++ app.py
@@ -17,12 +17,15 @@ client = MongoClient('127.0.0.1', 27017)
 mongo_db = client.shiyanlou
 
 class File(db.Model):
+    
+    __tablename__ = 'files'
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     created_time = db.Column(db.DateTime)
     content = db.Column(db.Text)
 
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     category = db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
 
     def __init__(self, title, content, category, created_time=None):
@@ -39,33 +42,33 @@ class File(db.Model):
         return '<File %r>' % self.title
 
     def add_tag(self, tag_name):
-        file_item = mongo_db.file.find_one({'file_id': self.id})
+        file_item = mongo_db.files.find_one({'file_id': self.id})
         if file_item:
             tags = file_item['tags']
             if tag_name not in tags:
                 tags.append(tag_name)
-            mongo_db.file.update_one({'file_id': self.id}, {'$set': {'tags': tags}})
+            mongo_db.files.update_one({'file_id': self.id}, {'$set': {'tags': tags}})
         else:
             tags = [tag_name]
-            mongo_db.file.insert_one({'file_id': self.id, 'tags': tags})
+            mongo_db.files.insert_one({'file_id': self.id, 'tags': tags})
 
         return tags
 
         
     def remove_tag(self, tag_name):
-        file_item = mongo_db.file.find_one({'file_id': self.id})
+        file_item = mongo_db.files.find_one({'file_id': self.id})
         if file_item:
             tags = file_item['tags']
             if tag_name in tags:
                 tags.remove(tag_name)
                 new_tags = tags
-                mongo_db.file.update_one({'file_id': self.id},{'$set': {'tags': new_tags}})
+                mongo_db.files.update_one({'file_id': self.id},{'$set': {'tags': new_tags}})
                 return new_tags
             return []
 
     @property
     def tags(self):
-        file_item = mongo_db.file.find({'file_id': self.id})
+        file_item = mongo_db.files.find({'file_id': self.id})
         if file_item:
             print(file_item)
             return file_item['tags']
@@ -73,6 +76,9 @@ class File(db.Model):
             return []
 
 class Category(db.Model):
+
+    __tablename__ = 'categories'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
@@ -89,12 +95,8 @@ def not_found(error):
 
 @app.route('/')
 def index():
-    file = File.query.all()
-    for f in file:
-        tags = mongo_db.file.find({'id': f.id}).tags
-        f.tags = tags
     
-    return render_template('index.html', names=file)
+    return render_template('index.html', names=File.query.all())
 
 @app.route('/files/<file_id>')
 def file(file_id):
